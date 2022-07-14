0b15a09992a1716332afdd94b8b504fbac660d55
ulzcq <syj9913@ynu.ac.kr>
2022-07-14 23:14:25 +0900

    Feat: 서블릿 예외처리 - 오류 화면 제공
    서블릿이 제공하는 오류화면 기능을 사용해본다.
    지금은 스프링 부트를 통해서 서블릿 컨테이너를 실행하기 때문에, 스프링 부트 기능을 사용해 서블릿 오류 페이지를 등록한다.
    
    1. 컨트롤러에서 예외가 발생하면 WAS까지 거꾸로 올라가 전파되고,
    2. WAS는 오류 페이지 경로를 찾아서 호출한다.(이것을 웹 브라우저, 즉 클라이언트는 모른다)
    
    WAS는 오류정보까지 request의 attribute에 추가해서 넘겨주므로 오류 페이지에서 이것을 사용할 수 있다.
    
    Issues: #2

---
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,26 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        //404에러가 터지면 호출
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+
+        //500에러가 터지면 호출
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        //RuntimeException과 그 자식 Exception이 터지면 호출
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        //등록
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+    }
+}
