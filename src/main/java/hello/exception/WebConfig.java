860c750f26c2ebc8f395f6c65e4def77de17898d
ulzcq <syj9913@ynu.ac.kr>
2022-07-15 00:20:53 +0900

    Feat: 서블릿 예외 처리 - 필터
    오류 발생시 오류페이지 출력을 위해 필터나 인터셉트가 한번 더 호출되는 것은 비효율적이다.
    서블릿이 제공하는 DispatcherType라는 추가 정보를 사용해 정상요청인지 오류페이지 요청인지를 구분할 수 있다.
    filterRegistrationBean.setDispatcherTypes(파라미터 값); 을 사용한다.
    아무 것도 넣지 않으면 기본 값이 DispatcherType.REQUEST 이므로 클라이언트 요청이 있는 경우에만 필터가 적용된다.
    오류 페이지 요청 전용 필터를 적용하고 싶으면 DispatcherType.ERROR만 지정하면 된다.
    
    Isseus: #3

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST); // (Default) 클라이언트의 요청이 있는 경우에만 필터가 적용된다
+        return filterRegistrationBean;
+    }
+}
